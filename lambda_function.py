135b292ee18a8b90376fd58c33df9edbd1b93728
Janvi Siddhpura <77524929+janvisiddhpura@users.noreply.github.com>
2025-06-16 20:30:53 +0530

    Files added

---
+++ lambda_function.py
@@ -0,0 +1,57 @@
+import json
+import os
+import boto3
+import urllib.parse
+
+def lambda_handler(event, context):
+    try:
+        mypage = page_router(event['httpMethod'], event['queryStringParameters'], event['body'])
+        return mypage
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
+
+def page_router(httpmethod, querystring, formbody):
+    if httpmethod == 'GET':
+        try:
+            with open('./contactus.html', 'r') as htmlFile:
+                htmlContent = htmlFile.read()
+            return {
+                'statusCode': 200,
+                'headers': {"Content-Type": "text/html"},
+                'body': htmlContent
+            }
+        except Exception as e:
+            return {
+                'statusCode': 500,
+                'body': json.dumps({'error': str(e)})
+            }
+
+    elif httpmethod == 'POST':
+        try:
+            insert_record(formbody)
+            with open('./success.html', 'r') as htmlFile:
+                htmlContent = htmlFile.read()
+            return {
+                'statusCode': 200,
+                'headers': {"Content-Type": "text/html"},
+                'body': htmlContent
+            }
+        except Exception as e:
+            return {
+                'statusCode': 500,
+                'body': json.dumps({'error': str(e)})
+            }
+
+def insert_record(formbody):
+    formbody = urllib.parse.unquote_plus(formbody)
+    formbody = formbody.replace("=", "' : '")
+    formbody = formbody.replace("&", "', '")
+    formbody = "INSERT INTO \"berry-contacts\" VALUE {'" + formbody + "'}"
+
+    client = boto3.client('dynamodb')
+    response = client.execute_statement(Statement=formbody)
+    # Assuming the execute_statement call returns successfully
+    return response
